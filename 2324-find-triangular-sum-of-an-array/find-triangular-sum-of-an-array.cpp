7867589b39daff29c1ba296c5d16575895e1bf3a
vikashkshukla123 <152985774+vikashkshukla123@users.noreply.github.com>
2025-09-30 14:13:40 +0530

    Time: 363 ms (19.82%) | Memory: 382.6 MB (19.25%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -1,14 +1,44 @@
+// class Solution {
+// public:
+//     int triangularSum(vector<int>& nums) {
+//         while(nums.size() > 1){
+//             vector<int>temp;
+//             for(int i = 0; i<nums.size() - 1; i++){
+//                 temp.push_back((nums[i] + nums[i+1]) % 10);
+//             }
+//             nums = move(temp);
+//         }
+//         return nums[0];
+        
+//     }
+// };
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        while(nums.size() > 1){
-            vector<int>temp;
-            for(int i = 0; i<nums.size() - 1; i++){
-                temp.push_back((nums[i] + nums[i+1]) % 10);
-            }
-            nums = move(temp);
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
         }
+        int ans ;
+        for(int i = 0; i<n-1; i++){
+            vector<int>a;
+            for(int j = 0; j<nums.size() - 1; j++){
+                a.push_back((nums[j]+nums[j+1]) % 10);
+               
+            }
+            nums = move(a);
+            
+
+
+        }
+
+
         return nums[0];
+
+    
+
+
+
         
     }
 };
