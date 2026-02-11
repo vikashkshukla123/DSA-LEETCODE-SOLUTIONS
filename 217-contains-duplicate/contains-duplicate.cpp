16353246df958324399ac13f8b5d683043088c9e
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-02-11 22:50:12 +0530

    Time: 67 ms (17.57%) | Memory: 93 MB (13.46%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(int i = 0; i<n; i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            if(it->second >= 2){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
