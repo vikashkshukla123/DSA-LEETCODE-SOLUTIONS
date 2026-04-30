682baff07850673b9e197065a5577bf208f096ab
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-04-30 09:39:02 +0530

    Time: 838 ms (59.41%) | Memory: 528.3 MB (11.85%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-int f(int i, int j, vector<vector<int>>&grid, int k,vector<vector<vector<int>>>&dp){
-    int n = grid.size();
-    int m = grid[0].size();
+int n,m;
+int solve(int row, int col, vector<vector<int>>&grid, int k,vector<vector<vector<int>>>&dp){
+    if(row >= n || col >= m) return -1e9;
     if(k < 0) return -1e9;
-    if(i == n || j == m) return -1e9;
-    if(i == n-1 && j == m-1){
-        if(min(1,grid[i][j]) <= k){
-            return grid[i][j];
+
+    
+    if(row == n-1 && col == m-1){
+        if(min(1,grid[row][col]) <= k){
+            return grid[row][col];
         }
         return -1e9;
     }
-    if(dp[i][j][k] != -1) return dp[i][j][k];
-
-       int bottom = grid[i][j] + f(i+1,j,grid, k - min(1, grid[i][j]),dp);
-    
-    
-       int right = grid[i][j] + f(i,j+1, grid, k - min(1,grid[i][j]),dp);
     
 
-    return dp[i][j][k] =  max(bottom, right);
+    if(dp[row][col][k] != -1){
+        return dp[row][col][k];
+    }
 
+
+    int case1 = grid[row][col] + solve(row+1,col, grid, k - min(1,grid[row][col]),dp);
+    int case2 = grid[row][col] + solve(row,col+1,grid,k - min(1,grid[row][col]),dp);
+
+    return dp[row][col][k] = max(case1,case2);
+
+    
+
+    
 }
     int maxPathScore(vector<vector<int>>& grid, int k) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(k+1,-1)));
+        n = grid.size();
+        m = grid[0].size();
 
-        int ans = f(0,0,grid, k,dp);
-        if(ans < 0) return -1;
-        return ans;
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
+
+        int ans = solve(0,0,grid,k,dp);
+        return ans < 0 ? -1 : ans;
+        
     }
 };
