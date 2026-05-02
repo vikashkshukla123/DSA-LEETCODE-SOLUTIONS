85c2ec30053ba09b489b3d249252cae3490a8adb
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-05-02 11:07:57 +0530

    Time: 15 ms (11.05%) | Memory: 10.3 MB (10.70%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-bool isMirror(int num, unordered_map<int,int>&mirror){
+bool isMirror(int num, unordered_map<int,int>&mp){
     int original = num;
     int numStart = 0;
-    bool changed = false;
-    while(num != 0){
-        int val = num %  10;
-        if(mirror.find(val) != mirror.end()){
-
-            if(val != mirror[val]){
-                changed = true;
-            }
-        }else{
-            return false;
-        }
-        numStart = numStart * 10 + mirror[val];
-        num = num / 10;
+    string original_num = to_string(num);
+    for(int i = 0; i<original_num.length(); i++){
+        int val = original_num[i] - '0';
+        if(mp.find(val) == mp.end()) return false;
+        numStart = numStart * 10 + mp[val];
     }
-    return changed;
-    
+    return (original != numStart);
 }
     int rotatedDigits(int n) {
         unordered_map<int,int>mirror;
