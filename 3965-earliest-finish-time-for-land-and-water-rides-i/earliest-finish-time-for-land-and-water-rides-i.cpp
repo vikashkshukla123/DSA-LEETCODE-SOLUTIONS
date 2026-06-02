34d59eefa5bbee65647e55e8a9d4970007126c7e
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-06-02 12:26:32 +0530

    Time: 20 ms (8.44%) | Memory: 91.8 MB (85.65%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,64 +1,44 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration,
-                           vector<int>& waterStartTime, vector<int>& waterDuration) {
-
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int n = landStartTime.size();
         int m = waterStartTime.size();
-        int INF = 1e9;
 
-        int idx1 = -1;
-        int totalTime1 = INF;
-        for (int i = 0; i < n; i++) {
-            int time = landStartTime[i] + landDuration[i];
-            if (time < totalTime1) {
-                totalTime1 = time;
-                idx1 = i;
+        int mini1 = 1e9;
+        for(int i = 0; i < n; i++){
+            int landStart = landStartTime[i];
+            int duration = landDuration[i];
+
+            int totalTime = landStart + duration;
+
+            for(int j = 0; j < m; j++){
+                if(waterStartTime[j] < totalTime){
+                    mini1 = min(mini1,totalTime + waterDuration[j]);
+                }else{
+                    int waitTime = waterStartTime[j] - totalTime;
+                    mini1 = min(mini1, totalTime + waitTime + waterDuration[j]);
+                }
             }
         }
 
-        int time1 = landStartTime[idx1] + landDuration[idx1];
+        int mini2 = 1e9;
+        for(int i = 0; i < m; i++){
+            int waterStart = waterStartTime[i];
+            int duration = waterDuration[i];
 
-        int min_time_water = INF;
-        for (int i = 0; i < m; i++) {
-            int finish =
-                max(time1, waterStartTime[i]) + waterDuration[i];
+            int totalTime = waterStart + duration;
 
-            min_time_water = min(min_time_water, finish - time1);
-        }
-
-        if (min_time_water == INF)
-            time1 = INF;
-        else
-            time1 += min_time_water;
-
-        // -------- Water first --------
-        int idx2 = -1;
-        int totalTime2 = INF;
-        for (int i = 0; i < m; i++) {
-            int time = waterStartTime[i] + waterDuration[i];
-            if (time < totalTime2) {
-                totalTime2 = time;
-                idx2 = i;
+            for(int j = 0; j < n; j++){
+                if(landStartTime[j] < totalTime){
+                    mini2 = min(mini2, totalTime + landDuration[j]);
+                }else{
+                    int waitTime = landStartTime[j] - totalTime;
+                    mini2 = min(mini2, totalTime + waitTime + landDuration[j]);
+                }
             }
         }
+return min(mini1,mini2);
 
-        int time2 = waterStartTime[idx2] + waterDuration[idx2];
-
-        int min_time_land = INF;
-        for (int i = 0; i < n; i++) {
-
-            int finish =
-                max(time2, landStartTime[i]) + landDuration[i];
-
-            min_time_land = min(min_time_land, finish - time2);
-        }
-
-        if (min_time_land == INF)
-            time2 = INF;
-        else
-            time2 += min_time_land;
-
-        return min(time1, time2);
+        
     }
 };
