bee8485936d091370050f6562395fa543c7bbef4
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-07-19 10:28:35 +0530

    Time: 1 ms (31.67%) | Memory: 10.1 MB (7.86%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+         int n = s.length();
+        unordered_set<char>st;
+        unordered_map<char,int>lastIndex;
+        for(int i = n-1; i >= 0; i--){
+            if(lastIndex.find(s[i]) == lastIndex.end()){
+                lastIndex[s[i]] = i;
+            }else{
+                continue;
+            }
+    
+        }
+
+        stack<char>stc;
+        for(int i = 0; i < n; i++){
+            if(st.find(s[i]) != st.end()) continue;
+            if(stc.empty() || stc.top() < s[i]  && st.find(s[i]) == st.end()){
+                stc.push(s[i]);
+                st.insert(s[i]);
+                continue;
+            }
+
+            while(!stc.empty() && stc.top() > s[i]  && lastIndex.count(stc.top())  && lastIndex[stc.top()] > i){
+                
+                st.erase(stc.top());
+                stc.pop();
+            }
+            if(st.find(s[i]) == st.end()){
+                st.insert(s[i]);
+                stc.push(s[i]);
+            }
+            
+        }
+
+        string ans = "";
+        while(!stc.empty()){
+            ans += stc.top();
+            stc.pop();
+        }
+reverse(ans.begin(),ans.end());
+return ans;
+
+    }
+};
