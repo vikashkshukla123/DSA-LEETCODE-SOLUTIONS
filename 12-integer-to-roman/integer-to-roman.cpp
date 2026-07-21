91e65e0d0cedf85efd38cef3a249e6c2ee1b7c1e
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-07-21 12:56:53 +0530

    Time: 7 ms (28.84%) | Memory: 15.5 MB (5.14%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string>v1 = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        vector<int>v2 = {1000,900,500,400,100,90,50,40,10,9, 5,4,1};
-        string res = "";
+        vector<string>ones = {",", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        vector<string>tens = {",","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX","XC"};
+        vector<string>hundreds = {",", "C", "CC", "CCC","CD","D","DC","DCC","DCCC","CM"};
+        vector<string>thousands = {",","M","MM","MMM"};
 
-       while(num != 0){
-        for(int i = 0; i<v1.size(); i++){
-            if(num >= v2[i]){
-                res += v1[i];
-                num = num - v2[i];
-                break;
-                
-            }
+        string ans  =  (thousands[num/1000] + hundreds[(num % 1000) / 100] + tens[(num % 100)/10] + ones[(num % 10)]);
+
+
+       string result = "";
+       for(int i = 0; i < ans.length(); i++){
+        if(ans[i] == ','){
+            continue;
         }
+        result += ans[i];
        }
-       return res;
-        
+
+       return result;
     }
 };
