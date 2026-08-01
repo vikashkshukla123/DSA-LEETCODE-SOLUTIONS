d1416a5f96efcc0ee6c64942abc485c090442da0
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-01 19:38:30 +0530

    Time: 116 ms (10.37%) | Memory: 9.5 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -5,32 +5,24 @@ int solve(int left, int right, vector<int>&nums){
     if(left > right){
         return 0;
     }
-
-    if(left < 0 || right >= n){
+    if(left >= n || right < 0){
         return 0;
     }
-    int case1 = 0;
-    int case2 = 0;
-    int ans1 = 1e9;
-    int ans2 = 1e9;
-    case1 = nums[left] - min(ans1,solve(left + 1, right, nums));
-    case2 = nums[right] - min(ans2, solve(left, right - 1, nums));
 
+    int case1 = nums[left] + min(solve(left + 2,right,nums), solve(left + 1, right -1,nums));
+    int case2 = nums[right] + min(solve(left+1,right - 1,nums), solve(left, right - 2,nums));
 
     return max(case1,case2);
-
-
-    return case1;
-
-    
 }
     bool predictTheWinner(vector<int>& nums) {
-        int totalSum = 0;
         int n = nums.size();
-       
+        int totalScore = 0;
+        for(int i = 0; i < n; i++){
+            totalScore += nums[i];
+        }
+        int ans1 = solve(0,n-1,nums);
+        int ans2 = totalScore - ans1;
 
-        int ans = solve(0,n-1,nums);
-        return (ans >= 0);
-        
+        return (ans1 >= ans2);
     }
 };
