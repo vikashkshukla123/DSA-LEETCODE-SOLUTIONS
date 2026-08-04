2ba104815793f3bdf07dd34ca07c4786c76455e6
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-04 09:55:30 +0530

    Time: 3 ms (49.33%) | Memory: 33.7 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-bool findkr(int ele, vector<int>&nums){
-    for(int i = 0; i<nums.size(); i++){
-        if(nums[i] == ele){
-            return true;
-        }
-    }
-    return false;
-}
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int min_ele = nums[0];
-        int max_ele = nums[n-1];
         vector<int>ans;
-
-        for(int i = min_ele + 1; i<max_ele; i++){
-            bool find = findkr(i,nums);
-            if(!find){
+        sort(nums.begin(),nums.end());
+        int low = nums[0];
+        int high = nums[n-1];
+        unordered_set<int>st;
+        for(int i = 0; i < n; i++){
+            st.insert(nums[i]);
+        }
+        
+        for(int i = low + 1; i < high; i++){
+            if(st.find(i) == st.end()){
                 ans.push_back(i);
             }
         }
-        
-        if(ans.size() == 0) return {};
         return ans;
-
-        
-        
     }
 };
