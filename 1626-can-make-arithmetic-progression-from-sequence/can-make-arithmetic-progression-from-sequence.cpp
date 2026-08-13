267e71ec8ee65b257c0098bc6e2c6c0fb0e7dd0c
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-13 10:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (17.71%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        int diff = arr[1] - arr[0];
+        if(n == 2) return true;
+
+        for(int i = 2; i < n; i++){
+            if(arr[i] - arr[i-1] != diff){
+                return false;
+            }
+        }
+      return true;  
+    }
+};
