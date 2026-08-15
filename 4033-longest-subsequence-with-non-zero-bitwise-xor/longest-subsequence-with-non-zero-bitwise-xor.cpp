970cacc1280c8c46ab1f173ad4c876e5a9da6de1
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-15 15:41:48 +0530

    Time: 208 ms (5.26%) | Memory: 232 MB (1.97%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int>st;
-        int XOR = 0;
-        for(int i = 0; i<n; i++){
-            XOR ^= nums[i];
-            st.insert(nums[i]);
+        unordered_map<int,int>mp;
+        int whole_xor = 0;
+        for(int i = 0; i < n; i++){
+            whole_xor ^= nums[i];
+            mp[nums[i]]++;
         }
-        if(XOR != 0) return n;
-        else if (XOR == 0 && st.size() == 1 && *st.begin() == 0){
-            return 0;
-        }else{
-            return n-1;
-        }
-        return n;
 
+        if(mp.size() == 1 && mp.begin()->first == 0){
+            return 0;
+        };
+
+        if(whole_xor == 0){
+            return n-1;
+        }else{
+            return n;
+        }
+        
     }
- };
+};
