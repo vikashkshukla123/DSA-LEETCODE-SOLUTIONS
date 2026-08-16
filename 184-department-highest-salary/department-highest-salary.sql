d6cd237fdb517e844281d6e73907507a7abfd496
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-16 17:38:50 +0530

    Time: 1007 ms (31.54%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,11 +1,15 @@
 # Write your MySQL query statement below
 WITH cte AS
-(SELECT e.name AS Employee , e.salary AS Salary , d.name AS Department, MAX(e.salary) OVER(PARTITION BY e.departmentId) AS max_salary
-FROM Employee e
-LEFT JOIN Department d
-ON e.departmentId = d.Id)
+(SELECT*,DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) as rnk
+FROM Employee)
 
 
-SELECT Department, Employee, Salary
-FROM cte
-WHERE salary = max_salary
+SELECT d.name AS Department, c.name AS Employee , c.salary 
+FROM cte c
+LEFT JOIN
+Department d
+ON c.departmentId = d.id
+WHERE rnk = 1;
+
+
+
