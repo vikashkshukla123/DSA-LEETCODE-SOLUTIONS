1b103a736e22854c6f530a078195e2a6356cc078
VIKAS KUMAR SHUKLA <152985774+vikashkshukla123@users.noreply.github.com>
2026-08-16 19:53:03 +0530

    Time: 456 ms (14.88%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
 WITH cte AS
-(SELECT* ,LEAD(id) OVER(ORDER BY id) AS next,
-LAG(id) OVER(ORDER BY id) AS prev
+(SELECT*, LEAD(id) OVER(ORDER BY id) AS nxt_person, LAG(id) OVER(ORDER BY id) AS prev_person
 FROM Seat)
 
-SELECT CASE WHEN ((id%2 = 1) AND next IS NOT NULL) THEN next
-WHEN (id % 2 = 0) THEN prev
-ELSE id  END AS id, student
-FROM cte 
+SELECT  CASE WHEN id % 2 = 1 AND nxt_person IS NOT NULL THEN nxt_person WHEN id % 2 = 1 AND nxt_person IS NULL THEN id ELSE prev_person END AS id, student
+FROM cte
 ORDER BY id
-
